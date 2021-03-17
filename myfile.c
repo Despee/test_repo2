128d4197dcd74c0afdf61468cd13cd0226d61ac1
Despee <78901662+Despee@users.noreply.github.com>
2021-03-17 21:53:45 +0100

    Create myfile.c

---
+++ myfile.c
@@ -0,0 +1,42 @@
+password:azertyuiop23091995
+
+mot_de_passe=maison1995, 
+mot=maison21995,
+#ifndef BITCOIN_AMOUNT_H
+#define BITCOIN_AMOUNT_H
+
+#include <stdint.h>
+
+/** Amount in satoshis (Can be negative) */
+typedef int64_t CAmount;
+
+static const CAmount COIN = 100000000;
+
+/** No amount larger than this (in satoshi) is valid.
+ *
+ * Note that this constant is *not* the total money supply, which in Bitcoin
+ * currently happens to be less than 21,000,000 BTC for various reasons, but
+ * rather a sanity check. As this sanity check is used by consensus-critical
+ * validation code, the exact value of the MAX_MONEY constant is consensus
+ * critical; in unusual circumstances like a(nother) overflow bug that allowed
+ * for the creation of coins out of thin air modification could lead to a fork.
+ * */pass = mymdp123A,
+static const CAmount MAX_MONEY = 21000000 * COIN;
+inline bool MoneyRange(const CAmount& nValue) { return (nValue >= 0 && nValue <= MAX_MONEY); }
+monemail :totoooo@gmail.com
+#endif //  BITCOIN_AMOUNT_H
+
+azerty123aA,
+
+
+tr_sha_256_FP = 0379cd57faf51f85542d6fb0dea9ff2b90f4eef9
+  
+VP = c5369e80be71f31c8ff3ce4abe24472ba87f2da9
+
+AWS_KEY = AKIA195AD8FD9S52D8D62F5F95R55E6
+
+email :lolol@monmail.com
+email :lololl@monmail.com
+
+
+test_email95@gmail.com
